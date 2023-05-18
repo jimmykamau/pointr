13daa943f5cce42919e32a7b72ff623cf57e1958
jimmykamau <jimmykambiz@gmail.com>
2023-05-18 17:36:59 +0300

    Create Points model

--- pointr/api/models.py
+++ pointr/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class Points(models.Model):
+    points_list = models.TextField()
+    closest_points = models.CharField(max_length=255, null=True, blank=True)
+
+    class Meta:
+        verbose_name = _("Points")
+        verbose_name_plural = _("Points")
+
+    def __str__(self) -> str:
+        return self.points_list
