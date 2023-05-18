189ce1abd5d95dbc81afc4e43bf8102003fc6f39
jimmykamau <jimmykambiz@gmail.com>
2023-05-18 17:39:53 +0300

    Add Points URL

--- pointr/pointr/urls.py
+++ pointr/pointr/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import api.views as api_views
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", api_views.GetCreatePointsApiView.as_view(), name="points-api"),
 ]
